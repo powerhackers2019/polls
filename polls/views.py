1d4c9d0c8ba1cba817a3afb7038ed9fe61dd6b7b
Sean Fottrell <sfottrell@att.net>
2019-03-03 21:33:51 -0800

    changed greeting again

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, Sean. You're at the polls index.")
+    return HttpResponse("Hello, Tellim Drachir. You're at the polls index.")
